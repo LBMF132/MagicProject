8e96f38b123481509f090aa9136b2dc74d1215e7
LBMF132 <35092274+LBMF132@users.noreply.github.com>
2018-01-30 08:21:45 +0300

    Updated parser

--- ParserUtil.java
+++ ParserUtil.java
@@ -1,9 +1,34 @@
 
 public class ParserUtil {
+	public static String full() {
+		Cell currentCell = PrintMap.currentLevel.contents[Player.xPos][Player.yPos];
+		String toRet = "";
+		if (currentCell.beenThere) {
+			toRet += currentCell.beenText;
+		} else {
+			toRet += currentCell.normText;
+		}
+		toRet += "\nThese items are in the room:";
+		for (Item i : currentCell.cellItems) {
+			toRet += "\n";
+			toRet += i.getName();
+		}
+		toRet += "\nYou can go these directions:";
+		if (currentCell.options[0])
+			toRet += "\nNorth";
+		if (currentCell.options[1])
+			toRet += "\nEast";
+		if (currentCell.options[2])
+			toRet += "\nSouth";
+		if (currentCell.options[3])
+			toRet += "\nWest";
+		return toRet;
+	}
+
 	public static String parse(String s) {
 		FightUtil.handleMonster();
 		String toPrint = "emp-t";
-		String[] parts = s.toLowerCase().split(" ",2);
+		String[] parts = s.toLowerCase().split(" ", 2);
 		switch (parts[0]) {
 		case "exit":
 			System.out.println("Exiting");
@@ -14,36 +39,36 @@ public class ParserUtil {
 			if (parts.length > 0) {
 				switch (parts[1]) {
 				case "north":
-					if(PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[0]) {
+					if (PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[0]) {
 						Player.yPos++;
-						System.out.println( PrintMap.currentLevel.contents[Player.xPos][Player.yPos].normText);
-					}else {
+						System.out.println(PrintMap.currentLevel.contents[Player.xPos][Player.yPos].normText);
+					} else {
 						toPrint = "You cannot go north";
 					}
 					break;
 				case "east":
-					if(PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[1]) {
+					if (PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[1]) {
 						Player.xPos++;
 						return PrintMap.currentLevel.contents[Player.xPos][Player.yPos].normText;
-					}else {
+					} else {
 						toPrint = "You cannot go east";
 					}
 					break;
 				case "south":
-					if(PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[2]) {
+					if (PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[2]) {
 						Player.yPos--;
 						return PrintMap.currentLevel.contents[Player.xPos][Player.yPos].normText;
 
-					}else {
+					} else {
 						toPrint = "You cannot go south";
 					}
 					break;
 				case "west":
-					if(PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[3]) {
+					if (PrintMap.currentLevel.contents[Player.xPos][Player.yPos].options[3]) {
 						Player.xPos--;
 						return PrintMap.currentLevel.contents[Player.xPos][Player.yPos].normText;
 
-					}else {
+					} else {
 						toPrint = "You cannot go west";
 					}
 					break;
@@ -51,20 +76,20 @@ public class ParserUtil {
 			}
 			break;
 		case "take":
-			for(Item iT:PrintMap.currentLevel.contents[Player.xPos][Player.yPos].cellItems) {
-				if(iT.getName().equals(parts[1])) {
+			for (Item iT : PrintMap.currentLevel.contents[Player.xPos][Player.yPos].cellItems) {
+				if (iT.getName().equals(parts[1])) {
 					Player.inventory.add(iT);
 					PrintMap.currentLevel.contents[Player.xPos][Player.yPos].cellItems.remove(iT);
-					return ("Took "+iT.getName());
+					return ("Took " + iT.getName());
 				}
 			}
 			toPrint = "Cannot find that item";
 			break;
 		case "equip":
-			for(Item iT:Player.inventory) {
-				if(iT.getName().equals(parts[1])) {
-					if(iT instanceof Weapon) {
-						Player.setEquippedWeapon((Weapon)(iT));
+			for (Item iT : Player.inventory) {
+				if (iT.getName().equals(parts[1])) {
+					if (iT instanceof Weapon) {
+						Player.setEquippedWeapon((Weapon) (iT));
 						toPrint = "Equipped" + iT.getName();
 					}
 				}
@@ -75,10 +100,11 @@ public class ParserUtil {
 		}
 		return toPrint;
 	}
+
 	public static void handleMonster() {
-			for(Monster m:PrintMap.currentLevel.contents[Player.xPos][Player.yPos].cellMonsters) {
-				
-			}
-		
+		for (Monster m : PrintMap.currentLevel.contents[Player.xPos][Player.yPos].cellMonsters) {
+
+		}
+
 	}
 }
